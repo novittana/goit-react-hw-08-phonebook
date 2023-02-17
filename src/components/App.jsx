ba637a463ba118bf164326251c0f0aa633ffc404
Tetiana Novitska <novittana2410@gmail.com>
2023-02-17 21:31:31 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,35 @@
-import { Component } from "react";
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { ContactForm } from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import Contact from './Contact/Contact'
-import { Filter } from "./Filter/Filter";
+import { ContactForm } from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Contact from './Contact/Contact';
+import { Filter } from './Filter/Filter';
+import storage from './helpers/storage';
+
+// const contacts= [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: ''
+    contacts: [],
+    filter: '',
   };
 
-
   componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    console.log(contacts);
+    // const savedContacts =
+    //   JSON.parse(localStorage.getItem('contacts-list')) ?? [];
+  const savedContacts = storage.load('contacts-list') ?? []//contacts;
+    this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+     const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      storage.save(
+        'contacts-list', contacts);
+    }
   }
 
   addContact = contact => {
