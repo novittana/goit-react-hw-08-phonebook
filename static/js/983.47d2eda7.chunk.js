da4c436a536a15909a8593819bbddfeee32bd04f
novittana <107628111+novittana@users.noreply.github.com>
2023-03-03 10:38:07 +0000

    Deploying to gh-pages from @ novittana/goit-react-hw-08-phonebook@f777dcc4aae24f4b7d38bf32b64ca4938a274a8f 🚀

---
+++ static/js/983.47d2eda7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var o=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,o.jsx)("div",{style:i.container,children:(0,o.jsx)("h1",{style:i.title,children:"Welcome on Phonebook Home page"})})}}}]);
+//# sourceMappingURL=983.47d2eda7.chunk.js.map
